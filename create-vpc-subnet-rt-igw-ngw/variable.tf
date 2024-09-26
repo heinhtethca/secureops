aac0ad7c79f95e56e2f97db54bc23ce8372e047d
heinhtethca <heinhtet.hca@gmail.com>
2024-09-26 22:25:51 +0800

    private-db-subnets

--- create-vpc-subnet-rt-igw-ngw/variable.tf
+++ create-vpc-subnet-rt-igw-ngw/variable.tf
@@ -3,5 +3,13 @@ variable public_subnets {
   description = "public-subnets"
 }
 
+variable private_subnets {
+  default     = ["10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24"]
+  description = "private-subnets"
+}
 
+variable db_subnets {
+  default     = ["10.10.7.0/24", "10.10.8.0/24", "10.10.9.0/24"]
+  description = "db-subnets"
+}
 
