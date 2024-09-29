5824b2ecc92e213e93a1663ad33d98ed382879e1
heinhtethca <heinhtet.hca@gmail.com>
2024-09-29 22:24:41 +0800

    second commit

--- create-vpc-subnet-rt-igw-ngw/main.tf
+++ create-vpc-subnet-rt-igw-ngw/main.tf
@@ -1,7 +1,7 @@
-provider "aws" {
-  access_key = data.vault_aws_access_credentials.creds.access_key
-  secret_key = data.vault_aws_access_credentials.creds.secret_key
-}
+# provider "aws" {
+#   access_key = data.vault_aws_access_credentials.creds.access_key
+#   secret_key = data.vault_aws_access_credentials.creds.secret_key
+# }
 
 
 
