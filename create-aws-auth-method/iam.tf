d5a3603bcebb0e1911903741c0393e34b8b58633
heinhtethca <heinhtet.hca@gmail.com>
2024-11-12 22:40:38 +0800

    aws-auth-method

---
+++ create-aws-auth-method/iam.tf
@@ -0,0 +1,51 @@
+#create vault auth admin
+resource "aws_iam_user" "vault_auth_admin" {
+  name = "vault-auth-admin"
+  path = "/"
+
+  tags = {
+    name = "vault-auth-admin"
+  }
+}
+
+resource "aws_iam_access_key" "vault_auth_admin" {
+  user = aws_iam_user.vault_auth_admin.name
+}
+
+data "aws_iam_policy_document" "vault_auth_policy" {
+  statement {
+    sid    = "VaultAWSAuthMethod"
+    effect = "Allow"
+    actions = [
+        "ec2:DescribeInstances",
+        "iam:GetInstanceProfile",
+        "iam:GetUser",
+        "iam:ListRoles",
+        "iam:GetRole"
+     ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "vault_auth_policy" {
+  name   = "vault_auth_policy"
+  user   = aws_iam_user.vault_auth_admin.name
+  policy = data.aws_iam_policy_document.vault_auth_policy.json
+}
+
+resource "aws_iam_role" "vault_auth_aws_role" {
+  name               = "vault_auth_aws_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json # (not shown)
+
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
---
+++ create-aws-auth-method/main.tf
@@ -0,0 +1,51 @@
+resource "vault_auth_backend" "vault_auth_aws" {
+  type = "aws"
+
+}
+
+resource "vault_aws_auth_backend_client" "vault_auth_aws" {
+  backend    = vault_auth_backend.vault_auth_aws.path
+  access_key = aws_iam_access_key.ault_auth_admin.id
+  secret_key = aws_iam_access_key.ault_auth_admin.secret
+}
+
+resource "vault_policy" "db_policy" {
+  name = "db-policy"
+
+  policy = <<EOT
+path "auth/token/lookup-self" {
+  capabilities = ["read"]
+}
+# Allow tokens to renew themselves
+path "auth/token/renew-self" {
+    capabilities = ["update"]
+}
+# Allow tokens to revoke themselves
+path "auth/token/revoke-self" {
+    capabilities = ["update"]
+}
+path "db/" {
+  capabilities = ["read","list"]
+}
+path "db/*" {
+  capabilities = ["read","list"]
+}
+path "aws-master/" {
+  capabilities = ["read","list"]
+}
+path "aws-master/*" {
+  capabilities = ["read","list"]
+}
+EOT
+}
+
+
+resource "vault_aws_auth_backend_role" "vault_aws_auth_role" {
+  backend                         = vault_auth_backend.aws.path
+  role                            = "test-role"
+  auth_type                       = "iam"
+  bound_iam_principal_arns        = [aws_iam_role.vault_auth_aws_role.arn]
+  token_ttl                       = 300
+  token_max_ttl                   = 300
+  token_policies                  = [vault_policy.db_policy.name]
+}
