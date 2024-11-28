898336060287713540e3b07551f1e7cbb5e1da50
heinhtethca <heinhtet.hca@gmail.com>
2024-11-28 22:06:47 +0800

    updated-outpute-sensitive

--- create-approle-auth-method/output.tf
+++ create-approle-auth-method/output.tf
@@ -7,5 +7,6 @@ output "db_role_id" {
 output "db_secret_id" {
     description = "secret-id-for-db"
     value = vault_approle_auth_backend_role_secret_id.db_secret_id.secret_id
+    sensitive = true
   
 }
